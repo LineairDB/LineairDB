3511246b1703cae79438c149d58713719d8cb443
nikezono <nikezono@gmail.com>
2022-01-19 19:48:03 +0900

    use transaction's local epoch instead of global epoch

--- src/database_impl.h
+++ src/database_impl.h
@@ -119,16 +119,19 @@ class Database::Impl {
     }
   }
 
-  Transaction& BeginTransaction() { return *(new Transaction(this)); }
+  Transaction& BeginTransaction() {
+    epoch_framework_.MakeMeOnline();
+    return *(new Transaction(this));
+  }
 
   bool EndTransaction(Transaction& tx, CallbackType clbk) {
     if (tx.IsAborted()) {
       clbk(TxStatus::Aborted);
       delete &tx;
+      epoch_framework_.MakeMeOffline();
       return false;
     }
 
-    epoch_framework_.MakeMeOnline();
     bool committed = tx.Precommit();
     if (committed) {
       tx.tx_pimpl_->PostProcessing(TxStatus::Committed);
