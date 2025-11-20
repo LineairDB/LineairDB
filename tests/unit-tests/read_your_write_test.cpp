d25fe0feb11beb4d4396e879c06556cc2c68cadf
nikezono <nikezono@gmail.com>
2025-11-20 10:24:56 +0900

    Transaction#Scan now includes inserted keys

--- tests/unit-tests/read_your_write_test.cpp
+++ tests/unit-tests/read_your_write_test.cpp
@@ -20,35 +20,50 @@ class ReadYourWriteTest : public ::testing::Test {
   }
 };
 
-TEST_F(ReadYourWriteTest, ReadYourWriteInScan) {
+TEST_F(ReadYourWriteTest, ScanShouldIncludeInsertedKeys) {
   db_->CreateTable("users");
+  {
+    auto& tx = db_->BeginTransaction();
+    tx.SetTable("users");
+    tx.Write<int>("alice", 1);
+    const bool committed = db_->EndTransaction(tx, [](auto status) {
+      ASSERT_EQ(status, LineairDB::TxStatus::Committed);
+    });
+    ASSERT_TRUE(committed);
+    db_->Fence();
+  }
+
   constexpr int erin = 4;
   constexpr int bob = 5;
   constexpr int carol = 6;
 
-  auto& tx = db_->BeginTransaction();
-  tx.SetTable("users");
-  tx.Write<int>("erin", erin);
-  tx.Write<int>("bob", bob);
-  tx.Write<int>("carol", carol);
+  {
+    auto& tx = db_->BeginTransaction();
+    tx.SetTable("users");
+    tx.Write<int>("erin", erin);
+    tx.Write<int>("bob", bob);
+    tx.Write<int>("carol", carol);
+    auto count = tx.Scan<int>("alice", "erin", [&](auto key, auto value) {
+      if (key == "erin") {
+        EXPECT_EQ(value, erin);
+      }
+      if (key == "bob") {
+        EXPECT_EQ(value, bob);
+      }
+      if (key == "carol") {
+        EXPECT_EQ(value, carol);
+      }
+      return false;
+    });
+    ASSERT_TRUE(count.has_value());
+    ASSERT_EQ(count.value(), 4);
 
-  auto count = tx.Scan<int>("bob", "erin", [&](auto key, auto value) {
-    if (key == "erin") {
-      EXPECT_EQ(value, erin);
-    }
-    if (key == "bob") {
-      EXPECT_EQ(value, bob);
-    }
-    if (key == "carol") {
-      EXPECT_EQ(value, carol);
-    }
-    return false;
-  });
-  ASSERT_TRUE(count.has_value());
-  ASSERT_EQ(count.value(), 3);
-
-  const bool committed = db_->EndTransaction(tx, [](auto status) {
-    ASSERT_EQ(status, LineairDB::TxStatus::Committed);
-  });
-  ASSERT_TRUE(committed);
+    const bool committed = db_->EndTransaction(tx, [](auto status) {
+      ASSERT_EQ(status, LineairDB::TxStatus::Committed);
+    });
+    ASSERT_TRUE(committed);
+  }
 }
+
+// TODO: We should implement `ScanShouldExcludeDeletedKeys` test when the
+// lineairdb supports delete operation.
