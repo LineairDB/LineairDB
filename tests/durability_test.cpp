4034333445a7ee49a479231a9f13da25bfdcbb3d
nikezono <nikezono@gmail.com>
2026-06-24 14:50:18 +0900

    sleep for size-checking loop in test runner

--- tests/durability_test.cpp
+++ tests/durability_test.cpp
@@ -269,7 +269,7 @@ TEST_P(DurabilityTest, LogFileSizeIsBounded) {  // a.k.a., checkpointing
     assert(begin < now);
     size_t elapsed =
         std::chrono::duration_cast<std::chrono::seconds>(now - begin).count();
-    if (config.checkpoint_period * 10 < elapsed) break;
+    if (config.checkpoint_period * 20 < elapsed) break;
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
   ASSERT_FALSE(filesize_is_monotonically_increasing);
